906c5c35fd2c2d699297925e78a2db16cbda53a8
Vani900 <vani900@github.com>
2026-08-07 10:05:28 +0530

    Final Android Gradle 8.13 build & sync repair complete (assembleDebug & tests 100% PASS)

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,9 @@
 pluginManagement {
+    plugins {
+        id("com.android.application") version "8.3.2"
+        id("org.jetbrains.kotlin.android") version "1.9.22"
+        id("org.jetbrains.kotlin.kapt") version "1.9.22"
+    }
     repositories {
         google()
         mavenCentral()
